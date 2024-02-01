bddff29e4e74db313941686dc01bc5363ec13491
yss9358 <93y2s@naver.com>
2024-02-01 10:38:31 +0900

    20240201 관리자-회원관리 일단완성

--- src/com/javaex/projectMySQL/ManagerApp.java
+++ src/com/javaex/projectMySQL/ManagerApp.java
@@ -11,56 +11,89 @@ public class ManagerApp {
 
 		String memberId;
 		String menuNo;
-		String str;
+		boolean run = true;
 
-		while (true) {
+		exit: while (true) {
 			System.out.println("-------------------------------------------------------------");
 			System.out.println("1.근태관리 2.부서관리 3.회원관리 ");
 			System.out.println("(/q 입력시 프로그램 종료)");
 			System.out.println("-------------------------------------------------------------");
 			System.out.print("선택>");
 			String menuSelect = sc.nextLine();
-			
+
 			if (menuSelect.equals("/q")) {
-				System.out.println("프로그램종료");
-				break;
+				break exit;
 			}
-			
+
 			switch (menuSelect) {
 			case "1":
 				System.out.println("<근태관리 프로그램>");
 				break;
-				
-			case "2" :
+
+			case "2":
 				System.out.println("<부서관리 프로그램>");
 				break;
-			
-			case "3" :
-				System.out.println("<회원관리 프로그램 접속>");
-				System.out.println("-------------------------------------------------------------");
-				managerDao.memberList();
-				System.out.println("-------------------------------------------------------------");
-				System.out.print("수정할 직원번호 선택>");
-				// 1~직원아이디 수까지 for 문? (-1 이면 뒤로가기 /q 입력 종료)
-				// 여기까지 반복되게 해야됨
-				memberId = sc.nextLine(); // 직원번호
-				System.out.println("1.아이디 2.이름 3.전화번호 4.생년월일 5.입사일 6.부서번호 7.부서명 8.직급");
-				System.out.print("수정할 메뉴 선택>"); 
-				//1~8이면 진행 (-1이면 뒤로가기 /q입력 종료)
-				menuNo = sc.nextLine(); // 수정할 메뉴
-				managerDao.memberUpdate(memberId, menuNo);
-				System.out.println("수정이 완료되었습니다.");
-				break;
+
+			case "3":
 				
-			default :
-					System.out.println("다른번호를 입력하세요");
+				while (run) {
+					System.out.println("<회원관리 프로그램 접속>");
+					System.out.println("-------------------------------------------------------------");
+					managerDao.memberList();
+					System.out.println("-------------------------------------------------------------");
+					System.out.print("수정할 직원번호 선택>");
+					memberId = sc.nextLine(); // 직원번호
+					// 1~직원아이디 수까지 for 문? (-1 이면 뒤로가기 /q 입력 종료)
+					// 여기까지 반복되게 해야됨
+					if(memberId.equals("/q")) {
+						break exit;
+					}
+
+					System.out.println("1.아이디 2.이름 3.전화번호 4.생년월일 5.입사일 6.부서번호 7.부서명 8.직급");
+					System.out.print("수정할 메뉴 선택>");
+
+					// 1~8이면 진행 (-1이면 뒤로가기 /q입력 종료)
+					menuNo = sc.nextLine(); // 수정할 메뉴
+					switch (menuNo) {
+					case "1":
+					case "2":
+					case "3":
+					case "4":
+					case "5":
+					case "6":
+					case "7":
+					case "8":
+						managerDao.memberUpdate(memberId, menuNo);
+						System.out.println("수정이 완료되었습니다.");
+						break;
+					case "-1":
+						break;
+					case "/q":
+						break exit;
+					default:
+						System.out.println("다른번호를 입력하세요");
+						break;
+
+					}
+				} //while문
+				break; // case3 break
+
+			default:
+				System.out.println("다른번호를 입력하세요");
 
 			}
-		
+
 		}
+
 		
+		System.out.println("종료");
 		sc.close();
 
 	}
 
+	
+	
+	
+	
+	
 }
